55f380d15670ca4e03e84e48b877cd8dcd72111d
Mohammad Jeragh <mjeragh@gmail.com>
2023-06-20 21:22:25 +0300

    Initial Commit

---
+++ MM3DM2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MM3DM2
+//
+//  Created by Mohammad Jeragh on 20/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MM3DM2/MM3DM2App.swift
@@ -0,0 +1,17 @@
+//
+//  MM3DM2App.swift
+//  MM3DM2
+//
+//  Created by Mohammad Jeragh on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MM3DM2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
