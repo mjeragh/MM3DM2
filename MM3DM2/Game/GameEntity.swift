acffaa7ef5681e3083d776dd4337cc0f460d1963
Mohammad Jeragh <mjeragh@gmail.com>
2023-12-09 13:58:02 +0300

    Adding gameEntity, just before I change game scene

---
+++ MM3DM2/Game/GameEntity.swift
@@ -0,0 +1,30 @@
+//
+//  GameEntity.swift
+//  MM3DM2
+//
+//  Created by Mohammad Jeragh on 09/12/2023.
+//
+
+import Foundation
+
+class GameEntity: Entity {
+    var entityID: Int
+    var components: [String: Component] = [:]
+
+    init(id: Int) {
+        self.entityID = id
+    }
+
+    func addComponent(_ component: Component) {
+        components[String(describing: type(of: component))] = component
+    }
+
+    func removeComponent(_ componentType: String) {
+        components.removeValue(forKey: componentType)
+    }
+
+    func getComponent<T: Component>(_ componentType: T.Type) -> T? {
+        let typeName = String(describing: componentType)
+        return components[typeName] as? T
+    }
+}
